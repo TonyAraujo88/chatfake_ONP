eeb6149478b143dc18ad73faf5ef0025d9d23fda
TonyAraujo88 <tonybugattieb@gmail.com>
2025-04-05 09:18:57 -0300

    javascript_chatfake

---
+++ src/script/script.js
@@ -0,0 +1,7 @@
+//DOMContentLoeaded
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Minha página carregou");
+
+    const inputMsg = document.querySelector
+                        document.querySelectorAll
+});
