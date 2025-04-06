ebe01096b961b08fbebbb691b0e17a84580b98ac
Antonio Araújo <134899443+TonyAraujo88@users.noreply.github.com>
2025-04-06 22:28:05 +0000

    buttonSend

--- src/script/script.js
+++ src/script/script.js
@@ -2,6 +2,30 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Minha página carregou");
 
-    const inputMsg = document.querySelector
-                        document.querySelectorAll
+    // Quando trabalhamos com ID utilizamos o #
+    // Quando trabalhamos com Classes utilizamos o .
+
+    const inputMsg = document.querySelector("#inputMensagem");
+        console.log(inputMsg);
+        
+    inputMsg.placeholder = "Digite sua mensagem";
+
+    const buttons = document.querySelectorAll(".cursor--pointer");
+        console.log(buttons);
+
+    const buttonSend = document.querySelector(".cursor--pointer[src*='send']");
+        console.log(buttonSend);
+
+    //buttonSend.classList.add("minha-classe-modulo-um")
+
+    buttonSend.addEventListener("click", () => {
+        const texto = inputMsg.value.trim();
+
+        if (texto === "") {
+            alert("Não possue mensagem ainda.");
+        }else {
+            alert(texto);
+        }
+               
+    });
 });
